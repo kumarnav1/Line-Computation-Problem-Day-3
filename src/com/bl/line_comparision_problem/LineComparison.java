8e7abf50ca6f1ea64ffce5832ed20d44ad93f1ce
nk400 <navkr007@gmail.com>
2022-05-26 15:32:01 +0530

    ADDED: Welcome message

---
+++ src/com/bl/line_comparision_problem/LineComparison.java
@@ -0,0 +1,8 @@
+package com.bl.line_comparision_problem;
+
+public class LineComparison {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Problem Project");
+    }
+}
